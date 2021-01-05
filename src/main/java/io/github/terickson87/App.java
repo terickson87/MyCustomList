ead61dbab5efab5b7fe4c153c94a0e5cb1117a27
Todd Erickson <twericks@usc.edu>
2021-01-05 10:34:26 -0700

    Added popping to App

--- src/main/java/io/github/terickson87/App.java
+++ src/main/java/io/github/terickson87/App.java
@@ -14,18 +14,42 @@ public class App
         intList.pushFront(0);
         intList.pushBack(3);
 
+        System.out.println("intList");
+        for (ListNode<Integer> elem : intList) {
+            System.out.println(elem.getData());
+        }
+
+        ListNode<Integer> temp = intList.popFront();
+        System.out.println("intList - Popped front");
+        System.out.println(temp.getData());
+        temp = intList.popBack();
+        System.out.println("intList - Popped back");
+        System.out.println(temp.getData());
+
+        System.out.println("intList - w/ Popped front and back");
+        for (ListNode<Integer> elem : intList) {
+            System.out.println(elem.getData());
+        }
+
         MyCustomList<String> stringList = new MyCustomList<>();
         stringList.pushFront("B");
         stringList.pushBack("C");
         stringList.pushFront("A");
         stringList.pushBack("D");
 
-        System.out.println("intList");
-        for (ListNode<Integer> elem : intList) {
+        System.out.println("stringList");
+        for (ListNode<String> elem : stringList) {
             System.out.println(elem.getData());
         }
 
-        System.out.println("stringList");
+        ListNode<String> temp2 = stringList.popFront();
+        System.out.println("stringList - Popped front");
+        System.out.println(temp2.getData());
+        temp2 = stringList.popBack();
+        System.out.println("stringList - Popped back");
+        System.out.println(temp2.getData());
+        
+        System.out.println("stringList - w/ Popped front and back");
         for (ListNode<String> elem : stringList) {
             System.out.println(elem.getData());
         }
